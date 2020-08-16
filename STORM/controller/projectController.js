d964474eeadafd28c30900c099fecfdaedbf0e5f
Say-young <seyoung990525@gmail.com>
2020-08-17 01:44:15 +0900

    [refactor] project 에러핸들링

--- STORM/controller/projectController.js
+++ STORM/controller/projectController.js
@@ -15,7 +15,6 @@ module.exports = {
           return;
         }
         
-        
         //랜덤 프로젝트 코드 생성 과정
         let possibleAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         let possibleNumber = "1234567890";
@@ -45,7 +44,6 @@ module.exports = {
         }while(flag === 1);
 
 
-
         //오늘 날짜 데이터 생성
         var date = new Date();
         var year = date.getFullYear();
@@ -61,18 +59,14 @@ module.exports = {
 
 
         //예외처리2 : project_idx가 제대로 생성되었는지 확인
-        const result = await ProjectDao.createProject(project_name, project_comment, project_code, project_date);
-        const projectIdx = result.insertId;
-      
+        const projectIdx = await ProjectDao.createProject(project_name, project_comment, project_code, project_date);
         if(projectIdx === -1) {
           return res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
         }
 
-        //프로젝트가 잘 생성 되었을 경우 아래에 host 정보 등록
-
+        //아래는 프로젝트가 잘 생성 되었을 경우 host 정보 등록 과정
         //project_participant table에 user 정보 추가
         const project_participant_idx = await ProjectDao.memberEnterProject(projectIdx, user_idx);
-
         if (project_participant_idx === -1) {
             return res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
         }
@@ -103,29 +97,24 @@ module.exports = {
 
         //예외처리2 : project_idx를 제대로 받아왔는지 확인
         const result = await ProjectDao.checkProjectIdx(project_code);
-        let project_idx;
-        try{
-            project_idx = result[0].project_idx;
-        }catch{
-            return res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
-        }
-
-        if (project_idx === undefined) {
+        const project_idx = result[0].project_idx;
+        if(project_idx === -1){
             return res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
         }
 
         // [test] 예외처리: 이미 디비에 참여된 참가자는 거절 ㅋ
         const check_overlap_participants = await ProjectDao.testErrProject(user_idx, project_idx);
-        console.log(check_overlap_participants[0]["COUNT(*)"])
         if(check_overlap_participants[0]["COUNT(*)"] >= 1){
             return res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.TEST_ERROR));
         }
 
         //예외처리3 : project가 진행중일 경우 참여할 수 없음
-        const projectStatus = await ProjectDao.checkProjectStatus(project_idx);
-        const project_status = projectStatus[0].project_status;
+        const checkStatus = await ProjectDao.checkProjectStatus(project_idx);
+        const project_status = checkStatus[0].project_status;
         if (project_status == 1) {
             return res.status(statusCode.CANNOT_JOIN).send(util.fail(statusCode.CANNOT_JOIN, resMessage.JOIN_PROJECT_FAIL));
+        }else if(project_status === -1){
+            return res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
         }
 
         //예외처리4 : projectParticipantIdx가 제대로 존재하는지 확인
@@ -133,7 +122,6 @@ module.exports = {
         if (projectParticipantIdx === -1) {
             return res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
         }
-
 
         //프로젝트 참여 성공
         res.status(statusCode.OK).send(util.success(statusCode.OK, resMessage.JOIN_PROJECT_SUCCESS, {
@@ -151,7 +139,10 @@ module.exports = {
             return;
         }
 
-        var result = await ProjectDao.getProjectparticipant(project_idx);
+        const result = await ProjectDao.getProjectparticipant(project_idx);
+        if(result === -1){
+            return res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
+        }
 
         return res.status(statusCode.OK)
             .send(util.success(statusCode.OK, resMessage.SHOW_PROJECT_PARTICIPANT_LIST_SUCCESS, result));
@@ -161,7 +152,14 @@ module.exports = {
     getProjectInfo: async (req, res) => {
         const project_idx = req.params.project_idx;
 
-        var result = await ProjectDao.getProjectInfo(project_idx);
+        if(!project_idx){
+            return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
+        }
+
+        const result = await ProjectDao.getProjectInfo(project_idx);
+        if(result === -1){
+            return res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
+        }
 
         return res.status(statusCode.OK)
             .send(util.success(statusCode.OK, resMessage.READ_PROJECT_INFO, {
@@ -185,24 +183,28 @@ module.exports = {
 
         // project_participant_idx 뽑아내기
         const result = await ProjectDao.checkProjectParticipantIdx(user_idx, project_idx);
+        if(result === -1){
+            return res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
+        }
         const project_participant_idx = result[0]["project_participant_idx"];
-
-        if (project_participant_idx === -1) {
-            res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NO_PROJECT_PARTICIPANT));
-            return;
+        if (project_participant_idx === undefined) {
+            return res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
         }
 
-        
         //만약 호스트일 경우 체크
         const ifHost = await ProjectDao.checkHost(project_participant_idx);
         if(ifHost === 1) {
             //방 안에 사람들이 더 있을 때 - 다른 사람에게 호스트를 넘김
             //방 안에 사람들이 없을 때 - 방이 터짐..?
+        }else if(ifHost === -1){
+            return res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
         }
         
-
         //위의 조건들을 모두 체크 후 삭제하기
         const fin = await ProjectDao.deleteProjectparticipant(project_participant_idx);
+        if(fin === -1){
+            return res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
+        }
 
         //삭제 성공
         return res.status(statusCode.OK)
@@ -220,6 +222,9 @@ module.exports = {
         }
 
         const result = await ProjectDao.getProjectInfoPopUp(project_code);
+        if(result === -1){
+            return res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
+        }
 
         return res.status(statusCode.OK)
             .send(util.success(statusCode.OK, resMessage.READ_PROJECT_INFO, {
@@ -232,8 +237,15 @@ module.exports = {
     //참여한 프로젝트 이름과 카드 반환
     showAllProject: async (req, res) => {
         const user_idx = req.params.user_idx;
+        if (!user_idx) {
+            res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
+            return;
+        }
 
-        var result_enterProject = await ProjectDao.getProjectIdxName(user_idx);
+        const result_enterProject = await ProjectDao.getProjectIdxName(user_idx);
+        if(result_enterProject === -1){
+            return res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
+        }
 
         var array = [];
         for (i = 0; i < result_enterProject.length; i++) {
@@ -243,6 +255,10 @@ module.exports = {
 
             var card_list = [];
             card_list_result = await ProjectDao.getProjectCard(result_enterProject[i]["project_idx"]);
+            if(card_list_result === -1){
+                return res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
+            }
+
             for (var j = 0; j < card_list_result.length; j++) {
                 var data2 = new Object();
                 data2.card_img = card_list_result[j]["card_img"];
@@ -261,8 +277,15 @@ module.exports = {
     //최종 프로젝트 정보 반환
     finalInfo: async (req, res) => {
         const project_idx = req.params.project_idx;
+        if (!user_idx) {
+            res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
+            return;
+        }
 
         const result = await ProjectDao.finalInfo(project_idx);
+        if(result === -1){
+            return res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
+        }
 
         return res.status(statusCode.OK)
             .send(util.success(statusCode.OK, resMessage.GET_FINAL_INFO_SUCCESS, result));
@@ -272,26 +295,49 @@ module.exports = {
     finalScrapList: async (req, res) => {
         const user_idx = req.params.user_idx;
         const project_idx = req.params.project_idx;
+        if (!user_idx || !project_idx) {
+            res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
+            return;
+        }
 
         const result = await ProjectDao.finalScarpList(user_idx, project_idx);
+        if(result === -1){
+            return res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
+        }
 
         return res.status(statusCode.OK)
             .send(util.success(statusCode.OK, resMessage.ROUND_FINALINFO_SUCCESS, result));
     },
     
+    //프로젝트 시작할 경우 project_status를 1로 변경해서 중간 입장 불가능하도록
     setProjectStatus: async (req, res) => {
         const project_idx = req.params.project_idx;
+        if (!project_idx) {
+            res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
+            return;
+        }
 
         const result = await ProjectDao.setProjectStatus(project_idx);
+        if(result === -1){
+            return res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
+        }
         
         return res.status(statusCode.OK)
             .send(util.success(statusCode.OK, resMessage.UPDATE_PROJECT_STATUS_SUCCESS));
     },
 
+    //프로젝트 종료 버튼 클릭 시 DB에서 project_code 삭제
     finishProject: async (req, res) => {
         const project_idx = req.body.project_idx;
+        if (!project_idx) {
+            res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
+            return;
+        }
 
         const result = await ProjectDao.finishProject(project_idx);
+        if(result === -1){
+            return res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
+        }
         
         return res.status(statusCode.OK)
             .send(util.success(statusCode.OK, resMessage.DELETE_PROJECT_CODE_SUCCESS));
--- STORM/dao/project.js
+++ STORM/dao/project.js
@@ -11,10 +11,12 @@ module.exports = {
         const query = `INSERT INTO project (${fields}) VALUES (${questions})`;
         try {
             const result = await pool.queryParamArr(query, values);
-            return result;
+            const insertId = result.insertId;
+            return insertId;
         } catch (err) {
             console.log('createProject ERROR : ', err);
-            throw err;
+            return -1;
+            //throw err;
         }
     },
 
@@ -24,7 +26,9 @@ module.exports = {
             const result = await pool.queryParam(query);
             return result;
         }catch(err){
-            console.log(err)
+            console.log('getProjectInfoPopUp ERROR : ', err);
+            return -1;
+            //throw err;
         }
     },
 
@@ -36,7 +40,8 @@ module.exports = {
             return result;
         } catch (err) {
             console.log('checkProjectIdx ERROR : ', err);
-            throw err;
+            return -1;
+            //throw err;
         }
     },
 
@@ -48,7 +53,8 @@ module.exports = {
             return result;
         } catch (err) {
             console.log('checkProjectStatus ERROR : ', err);
-            throw err;
+            return -1;
+            //throw err;
         }
     },
 
@@ -64,7 +70,8 @@ module.exports = {
             return insertId;
         } catch (err) {
             console.log('memberEnterProject ERROR : ', err);
-            throw err;
+            return -1;
+            //throw err;
         }
     },
 
@@ -80,7 +87,8 @@ module.exports = {
             return insertId;
         } catch (err) {
             console.log('hostEnterProject ERROR : ', err);
-            throw err;
+            return -1;
+            //throw err;
         }
     },
 
@@ -92,7 +100,8 @@ module.exports = {
             return result;
         } catch (err) {
             console.log('getProjectInfo ERROR : ', err);
-            throw err;
+            return -1;
+            //throw err;
         }
     },
 
@@ -106,7 +115,8 @@ module.exports = {
             return result;
         } catch (err) {
             console.log('getProjectparticipant ERROR : ', err);
-            throw err;
+            return -1;
+            //throw err;
         }
     },
 
@@ -120,7 +130,8 @@ module.exports = {
             return result;
         } catch (err) {
             console.log('checkProjectParticipantIdx ERROR : ', err);
-            throw err;
+            return -1;
+            //throw err;
         }
     },
 
@@ -134,7 +145,8 @@ module.exports = {
 
         } catch (err) {
             console.log('deleteProjectparticipant ERROR : ', err);
-            throw err;
+            return -1;
+            //throw err;
         }
 
     },
@@ -150,7 +162,8 @@ module.exports = {
 
         } catch (err) {
             console.log('checkHost ERROR : ', err);
-            throw err;
+            return -1;
+            //throw err;
         }
 
     },
@@ -165,7 +178,8 @@ module.exports = {
             return result;
         } catch (err) {
             console.log('getProjectIdx ERROR : ', err);
-            throw err;
+            return -1;
+            //throw err;
         }
     },
 
@@ -177,7 +191,8 @@ module.exports = {
             return result;
         } catch (err) {
             console.log('getProjectCard ERROR : ', err);
-            throw err;
+            return -1;
+            //throw err;
         }
     },
 
@@ -208,7 +223,8 @@ module.exports = {
             return data;
         } catch (err) {
             console.log('finalInfo ERROR : ', err);
-            throw err;
+            return -1;
+            //throw err;
         }
     },
 
@@ -244,7 +260,8 @@ module.exports = {
 
         } catch (err) {
             console.log('finalScarpList ERROR : ', err);
-            throw err;
+            return -1;
+            //throw err;
         }
     },
 
@@ -255,7 +272,8 @@ module.exports = {
             return result
         }catch(err){
             console.log('finalScarpList ERROR : ', err);
-            throw err;
+            return -1;
+            //throw err;
         }
     },
 
@@ -266,7 +284,8 @@ module.exports = {
             return result
         }catch(err){
             console.log('finalScarpList ERROR : ', err);
-            throw err;
+            return -1;
+            //throw err;
         }
     },
 
