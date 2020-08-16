774427987b605412a4164f7a34b868ce0d55480e
Choong <cndqjacndqja@naver.com>
2020-08-16 17:25:23 +0900

    [refactor] 회원가입 로그인 수정

--- STORM/controller/userController.js
+++ STORM/controller/userController.js
@@ -2,16 +2,17 @@ const util = require('../modules/util');
 const statusCode = require('../modules/statusCode');
 const resMessage = require('../modules/responseMessage');
 const UserDao = require('../dao/user');
+const encrypt  = require('../modules/crypto');
 
 module.exports = {
 
   signup: async (req, res) => {
     //1. request body에서 값을 읽어온다.
-    const { user_name, user_token_kakao, user_token_google } = req.body;
+    const { user_name, user_email, user_password, user_img_flag } = req.body;
     const user_img = req.file.location;
 
     //예외처리1 : parameter 체크 - 하나라도 null이나 undefined가 들어올 경우
-    if (!user_name || (!user_token_kakao && !user_token_google) || user_img === undefined) {
+    if (!user_name || (!user_email && !user_password) || user_img === undefined) {
       res.status(statusCode.BAD_REQUEST).sendzzz(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE)); //400은 요청이 잘못됐다는 오류 메세지
       return;
     }
@@ -31,12 +32,34 @@ module.exports = {
     */
 
     //2. 새로운 User를 등록한다.
-    const userIdx = await UserDao.signup(user_name, user_token_kakao, user_token_google, user_img);
-    if (userIdx === -1) {
+    const {salt, hashed} = await encrypt.encrypt(user_password);
+
+    const idx = await UserDao.signup(user_name, user_email, hashed, salt, user_img, user_img_flag);
+    if (idx === -1) {
       return res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
     }
 
+
+
     //3. 가입성공
-    res.status(statusCode.OK).send(util.success(statusCode.OK, resMessage.CREATED_USER, userIdx));
+    res.status(statusCode.OK).send(util.success(statusCode.OK, resMessage.CREATED_USER, idx));
+  },
+
+  signIn: async(req, res) => {
+    const { user_email, user_password } = req.body;
+
+    if(!user_email || !user_password){
+      return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
+    }
+
+    const{salt, hashed} = await encrypt.encrypt(user_password);
+
+    if(!salt || !hashed){
+      return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
+    }
+
+    const user_idx = await UserDao.signIn(user_email, salt, hashed);
+    res.status(statusCode.OK).send(util.success(statusCode.OK, resMessage.LOGIN_SUCCESS, user_idx));
+
   }
 }
--- STORM/dao/user.js
+++ STORM/dao/user.js
@@ -1,13 +1,34 @@
 const pool = require('../modules/pool');
+const encrypt = require('../modules/crypto');
 
 module.exports = {
 
     //회원가입
-    signup: async (user_name, user_token_kakao, user_token_google, user_img) => {
-        const fields = 'user_name, user_token_kakao, user_token_google, user_img';
-        const questions = `?, ?, ?, ?`;
-        const values = [user_name, user_token_kakao, user_token_google, user_img];
+    signup: async (user_name, user_email, user_password, salt, user_img, user_img_flag) => {
+        const fields = 'user_name, user_email, user_password, salt, user_img, user_img_flag';
+        const questions = `?, ?, ?, ?, ?, ?`;
+        const values = [user_name, user_email, user_password, salt, user_img, user_img_flag];
         const query = `INSERT INTO user (${fields}) VALUES (${questions})`;
+        try {
+            const result = await pool.queryParamArr(query, values);
+            const insertId = result.insertId;
+            return insertId;
+        } catch (err) {
+            if (err.errno == 1062) {
+                console.log('signup ERROR : ', err.errno, err.code);
+                return -1;
+            }
+            console.log('signup ERROR : ', err);
+            throw err;
+        }
+    },
+
+    signIn: async(user_email, salt, user_password) => {
+        const fields = 'user_email, salt, user_password';
+        const questions = `?, ?, ?`;
+        const values = [user_email, salt, user_password];
+        const query = `SELECT INTO user (${fields}) VALUES (${questions})`;
+
         try {
             const result = await pool.queryParamArr(query, values);
             const insertId = result.insertId;
