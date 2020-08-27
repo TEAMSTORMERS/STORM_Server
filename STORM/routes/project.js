7c66195dead03478cadca083189d3ff6d2eed86a
Say-young <seyoung990525@gmail.com>
2020-08-28 00:27:40 +0900

    [feat] 프로젝트 나가기 api 추가

--- STORM/routes/project.js
+++ STORM/routes/project.js
@@ -2,19 +2,18 @@ var express = require('express');
 var router = express.Router();
 const projectController = require('../controller/projectController');
 
-
 router.get('/user/:user_idx', projectController.showAllProject);
 router.get('/info/:project_code', projectController.getProjectInfoPopUp);
 router.post('/enter', projectController.memberEnterProject);
 router.post('/', projectController.createProject); 
 router.get('/:project_idx', projectController.getProjectInfo);
+router.delete('/leave/:project_idx', projectController.deleteProject);
 router.put('/status/:project_idx', projectController.setProjectStatus);
 router.put('/finish/:project_idx', projectController.finishProject);
 router.get('/finalInfo/:project_idx', projectController.finalInfo);
 router.get('/finalScrapList/:user_idx/:project_idx', projectController.finalScrapList);
 
 //사용안함
-router.get('/enter/:project_idx', projectController.getProjectparticipant);
-//router.delete('/:user_idx/:project_idx', projectController.deleteProjectparticipant);
+//router.get('/enter/:project_idx', projectController.getProjectparticipant);
 
 module.exports = router;
